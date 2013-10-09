2a9a5fbb8ff19ebe60bf6d3b3894150f6c052a96
openbox00 <openbox00.tw@gmail.com>
2013-10-09 13:25:08 +0800

    use school5510587's shell can eazy to extend command

--- myshell.c
+++ myshell.c
@@ -1,3 +1,6 @@
+/*
+* This Shell is ref School5510587,and separate shell function is ref zzz0072
+*/
 #include "stm32f10x.h"
 #include "RTOSConfig.h"
 
@@ -5,24 +8,186 @@
 
 #include <stddef.h>
 #include "systemdef.h"
+#include <ctype.h> 
 
 extern struct task_info task_info_t;
 
-struct command_history {
-   char command_history1[100];
-   char command_history2[100];
+#define MAX_CMDNAME 19
+#define MAX_ARGC 19
+#define MAX_CMDHELP 1023
+#define HISTORY_COUNT 20
+#define CMDBUF_SIZE 100
+#define MAX_ENVCOUNT 30
+#define MAX_ENVNAME 15
+#define MAX_ENVVALUE 127
+#define STACK_SIZE 512 /* Size of task stacks in words */
+#define TASK_LIMIT 8  /* Max number of tasks we can handle */
+#define PIPE_BUF   64 /* Size of largest atomic pipe message */
+#define PATH_MAX   32 /* Longest absolute path */
+#define PIPE_LIMIT (TASK_LIMIT * 2)
+
+#define PATHSERVER_FD (TASK_LIMIT + 3) 
+	/* File descriptor of pipe to pathserver */
+
+#define PRIORITY_DEFAULT 20
+#define PRIORITY_LIMIT (PRIORITY_DEFAULT * 2 - 1)
+
+#define TASK_READY      0
+#define TASK_WAIT_READ  1
+#define TASK_WAIT_WRITE 2
+#define TASK_WAIT_INTR  3
+#define TASK_WAIT_TIME  4
+
+#define S_IFIFO 1
+#define S_IMSGQ 2
+
+#define O_CREAT 4
+
+char next_line[3] = {'\n','\r','\0'};
+char cmd[HISTORY_COUNT][CMDBUF_SIZE];
+int cur_his=0;
+int fdout;
+int fdin;
+
+/* Command handlers. */
+void export_envvar(int argc, char *argv[]);
+void show_echo(int argc, char *argv[]);
+void show_cmd_info(int argc, char *argv[]);
+void show_task_info(int argc, char *argv[]);
+void show_man_page(int argc, char *argv[]);
+void show_history(int argc, char *argv[]);
+void semihost_function(int argc, char *argv[]);
+
+/* Enumeration for command types. */
+enum {
+	CMD_ECHO = 0,
+	CMD_EXPORT,
+	CMD_HELP,
+	CMD_HISTORY,
+	CMD_MAN,
+	CMD_PS,
+	CMD_SYSCALL,
+	CMD_COUNT
+} CMD_TYPE;
+/* Structure for command handler. */
+typedef struct {
+	char cmd[MAX_CMDNAME + 1];
+	void (*func)(int, char**);
+	char description[MAX_CMDHELP + 1];
+} hcmd_entry;
+const hcmd_entry cmd_data[CMD_COUNT] = {
+	[CMD_ECHO] = {.cmd = "echo", .func = show_echo, .description = "Show words you input."},
+	[CMD_EXPORT] = {.cmd = "export", .func = export_envvar, .description = "Export environment variables."},
+	[CMD_HELP] = {.cmd = "help", .func = show_cmd_info, .description = "List all commands you can use."},
+	[CMD_HISTORY] = {.cmd = "history", .func = show_history, .description = "Show latest commands entered."}, 
+	[CMD_MAN] = {.cmd = "man", .func = show_man_page, .description = "Manual pager."},
+	[CMD_PS] = {.cmd = "ps", .func = show_task_info, .description = "List all the processes."},
+	[CMD_SYSCALL] = {.cmd = "system", .func = semihost_function, .description = "do Host system call."}
 };
+
+/* Structure for environment variables. */
+typedef struct {
+	char name[MAX_ENVNAME + 1];
+	char value[MAX_ENVVALUE + 1];
+} evar_entry;
+evar_entry env_var[MAX_ENVCOUNT];
+int env_count = 0;
+
+enum HOST_SYSCALL {
+    HOSTCALL_OPEN        = 0x01,
+    HOSTCALL_CLOSE       = 0x02,
+    HOSTCALL_WRITEC      = 0x03,
+    HOSTCALL_WRITE0      = 0x04,
+    HOSTCALL_WRITE       = 0x05,
+    HOSTCALL_READ        = 0x06,
+    HOSTCALL_READC       = 0x07,
+    HOSTCALL_ISERROR     = 0x08,
+    HOSTCALL_ISTTY       = 0x09,
+    HOSTCALL_SEEK        = 0x0A,
+    HOSTCALL_FLEN        = 0x0C,
+    HOSTCALL_REMOVE      = 0x0E,
+    HOSTCALL_TMPNAM      = 0x0D,
+    HOSTCALL_RENAME      = 0x0F,
+    HOSTCALL_CLOCK       = 0x10,
+    HOSTCALL_TIME        = 0x11,
+    HOSTCALL_SYSTEM      = 0x12,
+    HOSTCALL_ERRNO       = 0x13,
+    HOSTCALL_GET_CMDLINE = 0x15,
+    HOSTCALL_HEAPINFO    = 0x16,
+    HOSTCALL_ELAPSED     = 0x30,
+    HOSTCALL_TICKFREQ    = 0x31
+};
+
+union param_t
+{
+    int   pdInt;
+    void *pdPtr;
+    char *pdChrPtr;
+};
+
+typedef union param_t param;
+static inline int host_call(enum HOST_SYSCALL action, void *arg)
+{
+    /* For Thumb-2 code use the BKPT instruction instead of SWI.
+     * Refer to:
+     * http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0471c/Bgbjhiea.html
+     * http://en.wikipedia.org/wiki/ARM_Cortex-M#Cortex-M4 */
+    int result;
+    __asm__( \
+      "bkpt 0xAB\n"\
+      "nop\n" \
+      "bx lr\n"\
+        :"=r" (result) ::\
+    );
+    return result;
+}
 
 void print(char *str){
 	write(mq_open("/tmp/mqueue/out", 0), str,strlen(str)+1);
 }
 
-void int2char(int x, char *str){
-	if(x<10){
-	str[0] = 48+x;
-	}else{
-	str[1] =  48+x%10;
-	str[0] =  48+x/10;
+void semihost_function(int argc, char *argv[]){
+	char *cmd= "ls"; //assign ls for host
+	param semi_param[3] = {
+        { .pdPtr = *cmd},
+        { .pdInt = 3 }
+   	 };
+    return host_call(HOSTCALL_SYSTEM, semi_param);
+}
+
+char *find_envvar(const char *name)
+{
+	int i;
+
+	for (i = 0; i < env_count; i++) {
+		if (!strcmp(env_var[i].name, name))
+			return env_var[i].value;
+	}
+
+	return NULL;
+}
+
+//export
+void export_envvar(int argc, char *argv[])
+{
+	char *found;
+	char *value;
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		value = argv[i];
+		while (*value && *value != '=')
+			value++;
+		if (*value)
+			*value++ = '\0';
+		found = find_envvar(argv[i]);
+		if (found)
+			strcpy(found, value);
+		else if (env_count < MAX_ENVCOUNT) {
+			strcpy(env_var[env_count].name, argv[i]);
+			strcpy(env_var[env_count].value, value);
+			env_count++;
+		}
 	}
 }
 
@@ -36,194 +201,310 @@ char *statetran(int i){
 	default:return "	Unknow";
 	}
 }
-void scan(char *x)
+
+//this function helps to show int
+char * itoa(int n, char *buffer)
 {
-	read(open("/dev/tty0/in", 0), x, 1);
+	if (n == 0)
+		*(buffer++) = '0';
+	else {
+		int f = 10000;
+
+		if (n < 0) {
+			*(buffer++) = '-';
+			n = -n;
+		}
+
+		while (f != 0) {
+			int i = n / f;
+			if (i != 0) {
+				*(buffer++) = '0'+(i%10);;
+			}
+			f/=10;
+		}
+	}
+	*buffer = '\0';
+	return *buffer;
 }
 
-void ps_task_info()
+//ps
+void show_task_info(int argc, char* argv[])
 {
-	int i = 0;
-	char str[2] = {0,0,0};
-	char str1[2] = {0,0,0};
+	int task_i;
 
-	for(i;i<=*(task_info_t.task_count);i++){
-		print("Task no =	");
-		int2char(i,str);
-		print(str);
-		print("	;state = ");
-		print(statetran(task_info_t.tasks[i].status));
-		print("	;priority = ");	
-		int2char((task_info_t.tasks[i].priority),str1);
-		print(str1);
-		print("\n\r\0");
+	for (task_i = 0; task_i < *(task_info_t.task_count); task_i++) {
+		char task_info_pid[2];
+		char task_info_priority[3];
+		print("Task no = ");
+		itoa(task_info_t.tasks[task_i].pid,task_info_pid);
+		print(&task_info_pid);
+		print("\t;Status = ");
+		print(statetran(task_info_t.tasks[task_i].status));
+		print("\t;Priority = ");
+		itoa(task_info_t.tasks[task_i].priority,task_info_priority);
+		print(&task_info_priority);
+		write(fdout, &next_line , 3);
+	}
+}
+
+//help
+void show_cmd_info(int argc, char* argv[])
+{
+	const char help_desp[] = "This system has commands as follow\n\r\0";
+	int i;
+
+	write(fdout, &help_desp, sizeof(help_desp));
+	for (i = 0; i < CMD_COUNT; i++) {
+		write(fdout, cmd_data[i].cmd, strlen(cmd_data[i].cmd) + 1);
+		write(fdout, ": ", 3);
+		write(fdout, cmd_data[i].description, strlen(cmd_data[i].description) + 1);
+		write(fdout, next_line, 3);
+	}
+}
+
+//echo
+void show_echo(int argc, char* argv[])
+{
+	const int _n = 1; /* Flag for "-n" option. */
+	int flag = 0;
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-n"))
+			flag |= _n;
+		else
+			break;
+	}
+
+	for (; i < argc; i++) {
+		write(fdout, argv[i], strlen(argv[i]) + 1);
+		if (i < argc - 1)
+			write(fdout, " ", 2);
+	}
+
+	if (~flag & _n)
+		write(fdout, next_line, 3);
+}
+
+//man
+void show_man_page(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 2)
+		return;
+
+	for (i = 0; i < CMD_COUNT && strcmp(cmd_data[i].cmd, argv[1]); i++)
+		;
+
+	if (i >= CMD_COUNT)
+		return;
+
+	write(fdout, "NAME: ", 7);
+	write(fdout, cmd_data[i].cmd, strlen(cmd_data[i].cmd) + 1);
+	write(fdout, next_line, 3);
+	write(fdout, "DESCRIPTION: ", 14);
+	write(fdout, cmd_data[i].description, strlen(cmd_data[i].description) + 1);
+	write(fdout, next_line, 3);
+}
+
+void show_history(int argc, char *argv[])
+{
+	int i;
+
+	for (i = cur_his + 1; i <= cur_his + HISTORY_COUNT; i++) {
+		if (cmd[i % HISTORY_COUNT][0]) {
+			write(fdout, cmd[i % HISTORY_COUNT], strlen(cmd[i % HISTORY_COUNT]) + 1);
+			write(fdout, next_line, 3);
+		}
+	}
+}
+
+int write_blank(int blank_num)
+{
+	char blank[] = " ";
+	int blank_count = 0;
+
+	while (blank_count <= blank_num) {
+		write(fdout, blank, sizeof(blank));
+		blank_count++;
+	}
+}
+
+void find_events()
+{
+	char buf[CMDBUF_SIZE];
+	char *p = cmd[cur_his];
+	char *q;
+	int i;
+
+	for (; *p; p++) {
+		if (*p == '!') {
+			q = p;
+			while (*q && !isspace(*q))
+				q++;
+			for (i = cur_his + HISTORY_COUNT - 1; i > cur_his; i--) {
+				if (!strncmp(cmd[i % HISTORY_COUNT], p + 1, q - p - 1)) {
+					strcpy(buf, q);
+					strcpy(p, cmd[i % HISTORY_COUNT]);
+					p += strlen(p);
+					strcpy(p--, buf);
+					break;
+				}
+			}
+		}
+	}
+}
+
+
+
+/* Fill in entire value of argument. */
+int fill_arg(char *const dest, const char *argv)
+{
+	char env_name[MAX_ENVNAME + 1];
+	char *buf = dest;
+	char *p = NULL;
+
+	for (; *argv; argv++) {
+		if (isalnum(*argv) || *argv == '_') {
+			if (p)
+				*p++ = *argv;
+			else
+				*buf++ = *argv;
+		}
+		else { /* Symbols. */
+			if (p) {
+				*p = '\0';
+				p = find_envvar(env_name);
+				if (p) {
+					strcpy(buf, p);
+					buf += strlen(p);
+					p = NULL;
+				}
+			}
+			if (*argv == '$')
+				p = env_name;
+			else
+				*buf++ = *argv;
+		}
+	}
+	if (p) {
+		*p = '\0';
+		p = find_envvar(env_name);
+		if (p) {
+			strcpy(buf, p);
+			buf += strlen(p);
+		}
+	}
+	*buf = '\0';
+
+	return buf - dest;
+}
+/* Split command into tokens. */
+char *cmdtok(char *cmd)
+{
+	static char *cur = NULL;
+	static char *end = NULL;
+	if (cmd) {
+		char quo = '\0';
+		cur = cmd;
+		for (end = cmd; *end; end++) {
+			if (*end == '\'' || *end == '\"') {
+				if (quo == *end)
+					quo = '\0';
+				else if (quo == '\0')
+					quo = *end;
+				*end = '\0';
+			}
+			else if (isspace(*end) && !quo)
+				*end = '\0';
+		}
+	}
+	else
+		for (; *cur; cur++)
+			;
+
+	for (; *cur == '\0'; cur++)
+		if (cur == end) return NULL;
+	return cur;
+}
+
+void check_keyword()
+{
+	char *argv[MAX_ARGC + 1] = {NULL};
+	char cmdstr[CMDBUF_SIZE];
+	char buffer[CMDBUF_SIZE * MAX_ENVVALUE / 2 + 1];
+	char *p = buffer;
+	int argc = 1;
+	int i;
+
+	find_events();
+	strcpy(cmdstr, cmd[cur_his]);
+	argv[0] = cmdtok(cmdstr);
+	if (!argv[0])
+		return;
+
+	while (1) {
+		argv[argc] = cmdtok(NULL);
+		if (!argv[argc])
+			break;
+		argc++;
+		if (argc >= MAX_ARGC)
+			break;
+	}
+
+	for(i = 0; i < argc; i++) {
+		int l = fill_arg(p, argv[i]);
+		argv[i] = p;
+		p += l + 1;
+	}
+
+	for (i = 0; i < CMD_COUNT; i++) {
+		if (!strcmp(argv[0], cmd_data[i].cmd)) {
+			cmd_data[i].func(argc, argv);
+			break;
+		}
+	}
+	if (i == CMD_COUNT) {
+		write(fdout, argv[0], strlen(argv[0]) + 1);
+		write(fdout, ": command not found", 20);
+		write(fdout, next_line, 3);
 	}
 }
 
 void shell()
 {
-	char str[100];
-	char ch;
-	char ins[100];
-	int curr_char=0;
-	int curr_ins=0;
-	int init = 0;
-	int history_count =1;
-	struct command_history history;
-	int a=0;
-	int b=0;
-	int a1=0;
-	int b1=0;	
+	char put_ch[2]={'0','\0'};
+	char *p = NULL;
+	int cmd_count = 0;
 
-	while(1){	
-		switch (init){
-		case 0x0:
-			print("shell>>");
-			init = 1;
-		break;
-		case 0x1:
-			curr_char=0;
-			scan(&ch);
-		if((ch==32)&&(curr_ins <= 0)){		//fix <space><command>
-			str[curr_char++] = ch;
-			ins[curr_ins++] = ch;
-			curr_ins--;
-		}	
-		else{	
-			//fix <backspace> but <up> <down> still fault
-			if((ch==127)&&(curr_ins <= 0)){  
-					str[curr_char++] = '\0';
+	fdout = mq_open("/tmp/mqueue/out", 0);
+	fdin = open("/dev/tty0/in", 0);
+
+	for (;; cur_his = (cur_his + 1) % HISTORY_COUNT) {
+		p = cmd[cur_his];
+		print("Shell>>");
+
+		while (1) {
+			read(fdin, put_ch, 1);
+
+			if (put_ch[0] == '\r' || put_ch[0] == '\n') {
+				*p = '\0';
+				write(fdout, next_line, 3);
+				break;
 			}
-			else {
-				if((ch==127)&&(curr_ins > 0)){		//fix <space> ...  <backspace> can work success
-					str[curr_char++] = '\b';
-					str[curr_char++] = ' ';
-					str[curr_char++] = '\b';
-					curr_ins--;
+			else if (put_ch[0] == 127 || put_ch[0] == '\b') {
+				if (p > cmd[cur_his]) {
+					p--;
+					write(fdout, "\b \b", 4);
 				}
-				else{
-					str[curr_char++] = ch;
-					ins[curr_ins++] = ch;
-				}
+			}
+			else if (p - cmd[cur_his] < CMDBUF_SIZE - 1) {
+				*p++ = put_ch[0];
+				write(fdout, put_ch, 2);
 			}
 		}
-			print(str);
-
-			if(ch=='\r'){
-				history_count++;
-				if(history_count%2 == 0){
-					for(a1;a1<100;a1++)
-					history.command_history1 [a1] = 0;
-					for(a;a<curr_ins;a++)
-					history.command_history1[a] = ins[a];
-				}
-				else{
-					for(b1;b1<100;b1++)
-					history.command_history2[b1] = 0;
-					for(b;b<curr_ins;b++)
-					history.command_history2[b] = ins[b];
-				}
-				
-				
-				//history
-				if(((ins[0]=='h') || (ins[0]=='H')) &&
-					((ins[1]=='i') || (ins[1]=='I')) &&
-					((ins[2]=='s') || (ins[2]=='S')) &&
-					((ins[3]=='t') || (ins[3]=='T')) &&
-					((ins[4]=='o') || (ins[4]=='O')) &&
-					((ins[5]=='r') || (ins[5]=='R')) &&
-					((ins[6]=='y') || (ins[6]=='Y')) &&
-					(ins[7]=='\r'))
-					{
-					print("\n\0");
-					print(&history.command_history2);
-					print("\n");
-					print(&history.command_history1);
-					print("\n\r\0");
-					}
-				//help HELP
-				else if(((ins[0]=='h') || (ins[0]=='H')) &&
-					((ins[1]=='e') || (ins[1]=='E')) &&
-					((ins[2]=='l') || (ins[2]=='L')) &&
-					((ins[3]=='p') || (ins[3]=='P')) &&
-					(ins[4]=='\r'))
-					{
-						print("\n hello          -- show welcome.\n\r");
-						print("\n echo <message> -- show the message you tape.\n\r");
-						print("\n ps             -- show system threads info. \n\r");
-						print("\n history        -- show tape before. \n\r");
-						print("\n ^_____^\n\r");
-						print("\0");
-					}
-					//hello	HELLO
-				else if(((ins[0]=='h') || (ins[0]=='H')) &&
-					((ins[1]=='e') || (ins[1]=='E')) &&
-					((ins[2]=='l') || (ins[2]=='L')) &&
-					((ins[3]=='l') || (ins[3]=='L')) &&
-					((ins[4]=='o') || (ins[4]=='O')) &&
-					(ins[5]=='\r'))
-					{
-						print("\n welcome !\n\r\0");
-					}
-					//echo	ECHO
-				else if(((ins[0]=='e') || (ins[0]=='E')) &&
-					((ins[1]=='c') || (ins[1]=='C')) &&
-					((ins[2]=='h') || (ins[2]=='H')) &&
-					((ins[3]=='o') || (ins[3]=='O')) &&
-					(ins[4]==32))
-					{
-							int i = 5;
-							int echocunt = 0;
-							char echo[100];
-							print("\n\0");
-							for (i;i<curr_ins;i++){
-								echo[echocunt++]=ins[i];
-							}
-							print(echo);
-							print("\n\0");
-					}
-					//ps	PS
-				else if(((ins[0]=='p') || (ins[0]=='P')) &&
-					((ins[1]=='s') || (ins[1]=='S')) &&
-					(ins[2]=='\r'))
-					{
-							print("\n");
-							ps_task_info();	
-					}	
-				else {
-					//when echo not echo<space>
-					if(((ins[0]=='e') || (ins[0]=='E')) &&
-					((ins[1]=='c') || (ins[1]=='C')) &&
-					((ins[2]=='h') || (ins[2]=='H')) &&
-					((ins[3]=='o') || (ins[3]=='O')))
-					{
-					print("\n you should tape echo <message> \n\r");
-					}
-					else	//no command
-					{
-					print("\n");
-					ins[curr_ins-1] = '\0';
-					print(ins);
-					print(": command not found\n\r");
-					}	
-					}
-				curr_ins=0;
-				init = 0;
-				a = 0;
-				b = 0;
-				a1=0;
-				b1=0;	
-			}
-			
-		break;	
-		default:
-			curr_ins=0;
-			init = 0;
-			a = 0;
-			b = 0;
-			a1=0;
-			b1=0;
-		
-		}
+		check_keyword();	
 	}
-}	
+}
+
