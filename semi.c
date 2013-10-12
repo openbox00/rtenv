725e22b5b35cdaf4171036e71b7407224f35a29c
openbox00 <openbox00.tw@gmail.com>
2013-10-12 23:39:24 +0800

    Bug:when add semifunction, ps command will be crush

--- semi.c
+++ semi.c
@@ -1,240 +1,106 @@
+
 #include "stm32f10x.h"
 #include "RTOSConfig.h"
 
 #include "syscall.h"
 
 #include <stddef.h>
-#include "systemdef.h"
 #include <ctype.h> 
 
-extern struct task_info task_info_t;
+#define MAX_CMDNAME_host 19
+#define MAX_ARGC_host 19
+#define MAX_CMDHELP_host 1023
+#define HISTORY_COUNT_host 20
+#define CMDBUF_SIZE_host 100
+#define MAX_ENVCOUNT_host 30
+#define MAX_ENVNAME_host 15
+#define MAX_ENVVALUE_host 127
 
-#define MAX_CMDNAME 19
-#define MAX_ARGC 19
-#define MAX_CMDHELP 1023
-#define HISTORY_COUNT 20
-#define CMDBUF_SIZE 100
-#define MAX_ENVCOUNT 30
-#define MAX_ENVNAME 15
-#define MAX_ENVVALUE 127
-#define STACK_SIZE 512 /* Size of task stacks in words */
-#define TASK_LIMIT 8  /* Max number of tasks we can handle */
-#define PIPE_BUF   64 /* Size of largest atomic pipe message */
-#define PATH_MAX   32 /* Longest absolute path */
-#define PIPE_LIMIT (TASK_LIMIT * 2)
 
-#define PATHSERVER_FD (TASK_LIMIT + 3) 
-	/* File descriptor of pipe to pathserver */
-
-#define PRIORITY_DEFAULT 20
-#define PRIORITY_LIMIT (PRIORITY_DEFAULT * 2 - 1)
-
-#define TASK_READY      0
-#define TASK_WAIT_READ  1
-#define TASK_WAIT_WRITE 2
-#define TASK_WAIT_INTR  3
-#define TASK_WAIT_TIME  4
-
-#define S_IFIFO 1
-#define S_IMSGQ 2
-
-#define O_CREAT 4
-
-char next_line[3] = {'\n','\r','\0'};
-char cmd[HISTORY_COUNT][CMDBUF_SIZE];
-int cur_his=0;
-int fdout;
-int fdin;
+char next_line_host[3] = {'\n','\r','\0'};
+char cmd_host[HISTORY_COUNT_host][CMDBUF_SIZE_host];
+int cur_his_host=0;
+int fdout_host;
+int fdout_host;
 
 /* Command handlers. */
-void export_envvar(int argc, char *argv[]);
-void show_echo(int argc, char *argv[]);
-void show_cmd_info(int argc, char *argv[]);
-void show_task_info(int argc, char *argv[]);
-void show_man_page(int argc, char *argv[]);
-void show_history(int argc, char *argv[]);
-void semihost_function(int argc, char *argv[]);
+#if 0
+void show_echo_host(int argc, char *argv[]);
+void show_cmd_info_host(int argc, char *argv[]);
+void show_history_host(int argc, char *argv[]);
+#endif
 
 /* Enumeration for command types. */
 enum {
-	CMD_ECHO = 0,
-	CMD_EXPORT,
-	CMD_HELP,
-	CMD_HISTORY,
-	CMD_MAN,
-	CMD_PS,
-	CMD_SYSCALL,
-	CMD_COUNT
-} CMD_TYPE;
+	CMD_ECHO_HOST = 0,
+	CMD_HELP_HOST,
+	CMD_HISTORY_HOST,
+	CMD_COUNT_HOST
+} CMD_TYPE_HOST;
+
 /* Structure for command handler. */
+
 typedef struct {
-	char cmd[MAX_CMDNAME + 1];
+	char cmd[MAX_CMDNAME_host + 1];
 	void (*func)(int, char**);
-	char description[MAX_CMDHELP + 1];
+	char description[MAX_CMDHELP_host + 1];
 } hcmd_entry;
-const hcmd_entry cmd_data[CMD_COUNT] = {
-	[CMD_ECHO] = {.cmd = "echo", .func = show_echo, .description = "Show words you input."},
-	[CMD_EXPORT] = {.cmd = "export", .func = export_envvar, .description = "Export environment variables."},
-	[CMD_HELP] = {.cmd = "help", .func = show_cmd_info, .description = "List all commands you can use."},
-	[CMD_HISTORY] = {.cmd = "history", .func = show_history, .description = "Show latest commands entered."}, 
-	[CMD_MAN] = {.cmd = "man", .func = show_man_page, .description = "Manual pager."},
-	[CMD_PS] = {.cmd = "ps", .func = show_task_info, .description = "List all the processes."},
-	[CMD_SYSCALL] = {.cmd = "system", .func = semihost_function, .description = "do Host system call."}
+
+#if 0
+hcmd_entry cmd_data_host[CMD_COUNT_HOST] = {
+	[CMD_ECHO_HOST] = {.cmd = "echo", .func = show_echo_host, .description = "Show words you input."},
+	[CMD_HELP_HOST] = {.cmd = "help", .func = show_cmd_info_host, .description = "List all commands you can use."},
+	[CMD_HISTORY_HOST] = {.cmd = "history", .func = show_history_host, .description = "Show latest commands entered."}
 };
-
-
+#endif
 
 
 /* Structure for environment variables. */
 typedef struct {
-	char name[MAX_ENVNAME + 1];
-	char value[MAX_ENVVALUE + 1];
+	char name[MAX_ENVNAME_host + 1];
+	char value[MAX_ENVVALUE_host + 1];
 } evar_entry;
-evar_entry env_var[MAX_ENVCOUNT];
-int env_count = 0;
+evar_entry env_var_host[MAX_ENVCOUNT_host];
+int env_count_host = 0;
 
-enum SemihostReasons {
-	// Standard ARM Semihosting Commands:
-	Semihost_SYS_OPEN   = 0x1,
-	Semihost_SYS_CLOSE  = 0x2,
-	Semihost_SYS_WRITE  = 0x5,
-	Semihost_SYS_READ   = 0x6,
-	Semihost_SYS_ISTTY  = 0x9,
-	Semihost_SYS_SEEK   = 0xa,
-	Semihost_SYS_ENSURE = 0xb,
-	Semihost_SYS_FLEN   = 0xc
-};
 
-static inline int SemihostCall(enum SemihostReasons reason, void *arg)
-{
-	#define AngelSWI 		0xAB
-	#define AngelSWIInsn	"bkpt"
-	int value;
-	asm volatile ("mov r0, %1; mov r1, %2; " AngelSWIInsn " %a3; mov %0, r0"
-			: "=r" (value) // Outputs
-			: "r" (reason), "r" (arg), "i" (AngelSWI) // Inputs
-			: "r0", "r1", "r2", "r3", "ip", "lr", "memory", "cc");
-	return value;
-}
-
-void print(char *str){
+void print_host(char *str){
 	write(mq_open("/tmp/mqueue/out", 0), str,strlen(str)+1);
 }
 
-void semihost_function(int argc, char *argv[]){
-	semishell();
-}
-
-char *find_envvar(const char *name)
+char *find_envvar_host(const char *name)
 {
 	int i;
 
-	for (i = 0; i < env_count; i++) {
-		if (!strcmp(env_var[i].name, name))
-			return env_var[i].value;
+	for (i = 0; i < env_count_host; i++) {
+		if (!strcmp(env_var_host[i].name, name))
+			return env_var_host[i].value;
 	}
 
 	return NULL;
 }
 
-//export
-void export_envvar(int argc, char *argv[])
-{
-	char *found;
-	char *value;
-	int i;
-
-	for (i = 1; i < argc; i++) {
-		value = argv[i];
-		while (*value && *value != '=')
-			value++;
-		if (*value)
-			*value++ = '\0';
-		found = find_envvar(argv[i]);
-		if (found)
-			strcpy(found, value);
-		else if (env_count < MAX_ENVCOUNT) {
-			strcpy(env_var[env_count].name, argv[i]);
-			strcpy(env_var[env_count].value, value);
-			env_count++;
-		}
-	}
-}
-
-char *statetran(int i){
-	switch(i){
-	case 0x0:return	"	TASK_READY";
-	case 0x1:return	"	TASK_WAIT_READ";
-	case 0x2:return	"	TASK_WAIT_WRITE";
-	case 0x3:return	"	TASK_WAIT_INTR";	
-	case 0x4:return	"	TASK_WAIT_TIME";
-	default:return "	Unknow";
-	}
-}
-
-//this function helps to show int
-char * itoa(int n, char *buffer)
-{
-	if (n == 0)
-		*(buffer++) = '0';
-	else {
-		int f = 10000;
-
-		if (n < 0) {
-			*(buffer++) = '-';
-			n = -n;
-		}
-
-		while (f != 0) {
-			int i = n / f;
-			if (i != 0) {
-				*(buffer++) = '0'+(i%10);;
-			}
-			f/=10;
-		}
-	}
-	*buffer = '\0';
-	return *buffer;
-}
-
-//ps
-void show_task_info(int argc, char* argv[])
-{
-	int task_i;
-
-	for (task_i = 0; task_i < *(task_info_t.task_count); task_i++) {
-		char task_info_pid[2];
-		char task_info_priority[3];
-		print("Task no = ");
-		itoa(task_info_t.tasks[task_i].pid,task_info_pid);
-		print(&task_info_pid);
-		print("\t;Status = ");
-		print(statetran(task_info_t.tasks[task_i].status));
-		print("\t;Priority = ");
-		itoa(task_info_t.tasks[task_i].priority,task_info_priority);
-		print(&task_info_priority);
-		write(fdout, &next_line , 3);
-	}
-}
-
 //help
-void show_cmd_info(int argc, char* argv[])
+void show_cmd_info_host(int argc, char* argv[])
 {
+#if 0
 	const char help_desp[] = "This system has commands as follow\n\r\0";
 	int i;
 
-	write(fdout, &help_desp, sizeof(help_desp));
-	for (i = 0; i < CMD_COUNT; i++) {
-		write(fdout, cmd_data[i].cmd, strlen(cmd_data[i].cmd) + 1);
-		write(fdout, ": ", 3);
-		write(fdout, cmd_data[i].description, strlen(cmd_data[i].description) + 1);
-		write(fdout, next_line, 3);
+	write(fdout_host, &help_desp, sizeof(help_desp));
+	for (i = 0; i < CMD_COUNT_HOST; i++) {
+		print_host(cmd_data_host[i].cmd);
+		print_host("	: ");
+		print_host(cmd_data_host[i].description);		
+		write(fdout_host, next_line_host, 3);
 	}
+#endif
 }
 
 //echo
-void show_echo(int argc, char* argv[])
+void show_echo_host(int argc, char* argv[])
 {
+#if 0
 	const int _n = 1; /* Flag for "-n" option. */
 	int flag = 0;
 	int i;
@@ -247,64 +113,34 @@ void show_echo(int argc, char* argv[])
 	}
 
 	for (; i < argc; i++) {
-		write(fdout, argv[i], strlen(argv[i]) + 1);
+		write(fdout_host, argv[i], strlen(argv[i]) + 1);
 		if (i < argc - 1)
-			write(fdout, " ", 2);
+			write(fdout_host, " ", 2);
 	}
 
 	if (~flag & _n)
-		write(fdout, next_line, 3);
+		write(fdout_host, next_line_host, 3);
+#endif
 }
 
-//man
-void show_man_page(int argc, char *argv[])
+void show_history_host(int argc, char *argv[])
 {
+#if 0
 	int i;
 
-	if (argc < 2)
-		return;
-
-	for (i = 0; i < CMD_COUNT && strcmp(cmd_data[i].cmd, argv[1]); i++)
-		;
-
-	if (i >= CMD_COUNT)
-		return;
-
-	write(fdout, "NAME: ", 7);
-	write(fdout, cmd_data[i].cmd, strlen(cmd_data[i].cmd) + 1);
-	write(fdout, next_line, 3);
-	write(fdout, "DESCRIPTION: ", 14);
-	write(fdout, cmd_data[i].description, strlen(cmd_data[i].description) + 1);
-	write(fdout, next_line, 3);
-}
-
-void show_history(int argc, char *argv[])
-{
-	int i;
-
-	for (i = cur_his + 1; i <= cur_his + HISTORY_COUNT; i++) {
-		if (cmd[i % HISTORY_COUNT][0]) {
-			write(fdout, cmd[i % HISTORY_COUNT], strlen(cmd[i % HISTORY_COUNT]) + 1);
-			write(fdout, next_line, 3);
+	for (i = cur_his_host + 1; i <= cur_his_host + HISTORY_COUNT_host; i++) {
+		if (cmd_host[i % HISTORY_COUNT_host][0]) {
+			write(fdout_host, cmd_host[i % HISTORY_COUNT_host], strlen(cmd_host[i % HISTORY_COUNT_host]) + 1);
+			write(fdout_host, next_line_host, 3);
 		}
 	}
+#endif
 }
 
-int write_blank(int blank_num)
+void find_events_host()
 {
-	char blank[] = " ";
-	int blank_count = 0;
-
-	while (blank_count <= blank_num) {
-		write(fdout, blank, sizeof(blank));
-		blank_count++;
-	}
-}
-
-void find_events()
-{
-	char buf[CMDBUF_SIZE];
-	char *p = cmd[cur_his];
+	char buf[CMDBUF_SIZE_host];
+	char *p = cmd_host[cur_his_host];
 	char *q;
 	int i;
 
@@ -313,10 +149,10 @@ void find_events()
 			q = p;
 			while (*q && !isspace(*q))
 				q++;
-			for (i = cur_his + HISTORY_COUNT - 1; i > cur_his; i--) {
-				if (!strncmp(cmd[i % HISTORY_COUNT], p + 1, q - p - 1)) {
+			for (i = cur_his_host + HISTORY_COUNT_host - 1; i > cur_his_host; i--) {
+				if (!strncmp(cmd_host[i % HISTORY_COUNT_host], p + 1, q - p - 1)) {
 					strcpy(buf, q);
-					strcpy(p, cmd[i % HISTORY_COUNT]);
+					strcpy(p, cmd_host[i % HISTORY_COUNT_host]);
 					p += strlen(p);
 					strcpy(p--, buf);
 					break;
@@ -329,9 +165,9 @@ void find_events()
 
 
 /* Fill in entire value of argument. */
-int fill_arg(char *const dest, const char *argv)
+int fill_arg_host(char *const dest, const char *argv)
 {
-	char env_name[MAX_ENVNAME + 1];
+	char env_name[MAX_ENVNAME_host + 1];
 	char *buf = dest;
 	char *p = NULL;
 
@@ -345,7 +181,7 @@ int fill_arg(char *const dest, const char *argv)
 		else { /* Symbols. */
 			if (p) {
 				*p = '\0';
-				p = find_envvar(env_name);
+				p = find_envvar_host(env_name);
 				if (p) {
 					strcpy(buf, p);
 					buf += strlen(p);
@@ -360,7 +196,7 @@ int fill_arg(char *const dest, const char *argv)
 	}
 	if (p) {
 		*p = '\0';
-		p = find_envvar(env_name);
+		p = find_envvar_host(env_name);
 		if (p) {
 			strcpy(buf, p);
 			buf += strlen(p);
@@ -371,7 +207,7 @@ int fill_arg(char *const dest, const char *argv)
 	return buf - dest;
 }
 /* Split command into tokens. */
-char *cmdtok(char *cmd)
+char *cmdtok_host(char *cmd)
 {
 	static char *cur = NULL;
 	static char *end = NULL;
@@ -399,81 +235,88 @@ char *cmdtok(char *cmd)
 	return cur;
 }
 
-void check_keyword()
+void check_keyword_host()
 {
-	char *argv[MAX_ARGC + 1] = {NULL};
-	char cmdstr[CMDBUF_SIZE];
-	char buffer[CMDBUF_SIZE * MAX_ENVVALUE / 2 + 1];
+	char *argv[MAX_ARGC_host + 1] = {NULL};
+	char cmdstr[CMDBUF_SIZE_host];
+	char buffer[CMDBUF_SIZE_host * MAX_ENVVALUE_host / 2 + 1];
 	char *p = buffer;
 	int argc = 1;
 	int i;
 
-	find_events();
-	strcpy(cmdstr, cmd[cur_his]);
-	argv[0] = cmdtok(cmdstr);
+	find_events_host();
+	strcpy(cmdstr, cmd_host[cur_his_host]);
+	argv[0] = cmdtok_host(cmdstr);
 	if (!argv[0])
 		return;
 
 	while (1) {
-		argv[argc] = cmdtok(NULL);
+		argv[argc] = cmdtok_host(NULL);
 		if (!argv[argc])
 			break;
 		argc++;
-		if (argc >= MAX_ARGC)
+		if (argc >= MAX_ARGC_host)
 			break;
 	}
 
 	for(i = 0; i < argc; i++) {
-		int l = fill_arg(p, argv[i]);
+		int l = fill_arg_host(p, argv[i]);
 		argv[i] = p;
 		p += l + 1;
 	}
 
-	for (i = 0; i < CMD_COUNT; i++) {
-		if (!strcmp(argv[0], cmd_data[i].cmd)) {
-			cmd_data[i].func(argc, argv);
-			break;
-		}
+	for (i = 0; i < CMD_COUNT_HOST; i++) {
+//		if (!strcmp(argv[0], cmd_data_host[i].cmd)) {
+//			cmd_data_host[i].func(argc, argv);
+//			break;
+//		}
 	}
-	if (i == CMD_COUNT) {
-		write(fdout, argv[0], strlen(argv[0]) + 1);
-		write(fdout, ": command not found", 20);
-		write(fdout, next_line, 3);
+	if (i == CMD_COUNT_HOST) {
+		write(fdout_host, argv[0], strlen(argv[0]) + 1);
+		write(fdout_host, ": command not found", 20);
+		write(fdout_host, next_line_host, 3);
 	}
 }
+
 
 void semishell()
 {
+
 	char put_ch[2]={'0','\0'};
 	char *p = NULL;
-	int cmd_count = 0;
 
-	fdout = mq_open("/tmp/mqueue/out", 0);
-	fdin = open("/dev/tty0/in", 0);
+	fdout_host = mq_open("/tmp/mqueue/out", 0);
+	fdout_host = open("/dev/tty0/in", 0);
 
-	for (;; cur_his = (cur_his + 1) % HISTORY_COUNT) {
-		p = cmd[cur_his];
-		print("\tSemiShell>>");
+	for (;; cur_his_host = (cur_his_host + 1) % HISTORY_COUNT_host) {
+		p = cmd_host[cur_his_host];
+		print_host("\r");
+		print_host("Semishell>>");
+	
 
 		while (1) {
-			read(fdin, put_ch, 1);
+			read(fdout_host, put_ch, 1);
 
 			if (put_ch[0] == '\r' || put_ch[0] == '\n') {
 				*p = '\0';
-				write(fdout, next_line, 3);
+				write(fdout_host, next_line_host, 3);
 				break;
 			}
 			else if (put_ch[0] == 127 || put_ch[0] == '\b') {
-				if (p > cmd[cur_his]) {
+				if (p > cmd_host[cur_his_host]) {
 					p--;
-					write(fdout, "\b \b", 4);
+					write(fdout_host, "\b \b", 4);
 				}
 			}
-			else if (p - cmd[cur_his] < CMDBUF_SIZE - 1) {
+			else if (p - cmd_host[cur_his_host] < CMDBUF_SIZE_host - 1) {
 				*p++ = put_ch[0];
-				write(fdout, put_ch, 2);
+				write(fdout_host, put_ch, 2);
 			}
 		}
-		check_keyword();	
+		
+		check_keyword_host();	
+
 	}
+
 }
+
